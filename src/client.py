7e6eefbbb9e4acb6e2a32ace299f166ca70580e4
koji sugano <suga@kojinoMacBook-Pro.local>
2023-05-04 01:52:25 +0900

    :bug: 誤字を修正

--- src/client.py
+++ src/client.py
@@ -6,7 +6,7 @@ def run():
     with grpc.insecure_channel('localhost:5001') as channel:
         stub = helloworld_pb2_grpc.HelloWorldServiceStub(channel)
         response = stub.SayHello(helloworld_pb2.HelloWorldRequest())
-    print("Reponse: %s" % response.message)
+    print("Response: %s" % response.message)
 
 if __name__ == '__main__':
     run()
